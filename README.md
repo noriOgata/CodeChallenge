4dc4f9e0e0e99830c6af7cb520b530cd2293d645
noriOgata <dacotahturner@gmail.com>
2015-03-27 05:30:28 -0700

    Added in some authentication and timeout signatures. I think I also got a little HMAC signing as well.

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # CodeChallenge
 To run the application:
 
-Open the Terminal and change directory to ../CodeChallenge
+Open the Terminal and change directory to 'CodeChallenge'
 
 Run the program with ./checkSum.py
 
-Open your preferred web browser and go to the address localhost:5000/createchecksum?url='your_url'
+Open your preferred web browser and enter the address for authenticate: localhost:5000/authenticate?username='any_name'. This is a faux authenticate so no password is needed. This will create a session that will last for 3 minutes before timing out.
 
-The resulting url should show in the browser copy the address.
+Next go to the address localhost:5000/createchecksum?url='your_url'
 
-then in the address bar or new tab go to localhost:5000/verifychecksum?url='url_created'
+The resulting url should show in the browser, copy the address.
 
-it will return a verified message if the checksum is valid if not you will get a bad request
+Then in the address bar or new tab go to localhost:5000/verifychecksum?url='url_created' this includes the added checksum argument. If the url does not match exactly with the original url it will return a 400 BAD REQUEST and if the checksum differs it will be a bad request.
+
+If you'd like to try again you can use the localhost:5000/logout to exit the current session and start over with a new user.
+
+Every user appends their username to the secret key when the check sum is created so as long as the usernames are unique so is the url and checksum.
--- checkSum.py
+++ checkSum.py
@@ -1,23 +1,53 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, request
+from datetime import timedelta
+from flask import Flask, session, escape, abort, request
 import hashlib, urllib
+
+def createUrlWithArgs(main, items):
+	for key, val in items:
+             if key != 'url' and key != 'checksum':
+                 s = ""
+                 s += str(key) + '=' + str(val)
+                 main += '&' + s
+	return main
+ 
 
 app = Flask(__name__)
 app.secret_key = 'S6I5sAR7JHWqQQ=='
+app.permanent_session_lifetime = timedelta(minutes=3)
+
+@app.route('/authenticate')
+def authenticate():
+	if request.args.has_key('username') == False:
+		abort(400)
+	elif 'name' in session:
+		return 'Already logged in as %s' % escape(session['name'])
+	
+	session.permanent = True
+	session['name'] = request.args.get('username')
+	return 'Authentification Successful!'
+
+@app.route('/logout')
+def logout():
+	session.pop('fullUrl', None)
+	session.pop('checkSum', None)
+	session.pop('name', None)
+
+	return 'Logged out successfully'
+
 @app.route('/createchecksum')
 def createCheckSum():
 	if request.args.has_key('url') == False:
 		abort(404)
-	mainUrl = request.args.get('url')
+	elif 'name' not in session:
+		return 'You have not been authenticated'
 	
-	for key, val in request.args.iteritems():
-		if key != 'url':
-			s = ""
-			s += str(key) + '=' + str(val)
-			mainUrl += '&' + s
+	mainUrl = createUrlWithArgs(request.args.get('url'), request.args.iteritems())	
 	
-	checkSum = hashlib.md5(app.secret_key).hexdigest()	
+	checkSum = hashlib.md5(app.secret_key + session['name']).hexdigest()	
 	returnUrl = mainUrl + "&checksum=" + checkSum
+	session['fullUrl'] = mainUrl
+	session['checkSum'] = checkSum
 	return returnUrl 
 
 @app.route('/verifychecksum')
@@ -26,9 +56,15 @@ def verifyCheckSum():
 		abort(404)
 	elif request.args.has_key('checksum') == False:
 		abort(400)
+	elif 'name' not in session:
+		return 'You have not been Authenticated'
+	
+	urlPath = createUrlWithArgs(request.args.get('url'), request.args.iteritems())
 
-	localCheckSum = hashlib.md5(secretKey).hexdigest()
-	if localCheckSum != request.args.get('checksum'):
+	if session['checkSum'] != request.args.get('checksum') or session['fullUrl'] != urlPath:
+		session.pop('fullUrl', None)
+		session.pop('checkSum', None)
+		session.pop('name', None)
 		abort(400)	
 	
 	return "checksum Verified!"
